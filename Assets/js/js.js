0f764d1f22c4e20440f46249d8b9c1ecff21d2c4
AgungWicaksonoPutro <wiputrosnc@gmail.com>
2020-08-03 07:05:15 +0700

    Fundamental HTML, CSS, Javascript

---
+++ Assets/js/js.js
@@ -0,0 +1,9 @@
+// let tryChart = document.getElementById('chart').getContext('2d');
+// let mychart = new Chart (tryChart, {
+//     type:'line',
+//     data: {
+//         labels:'This Month', 'Last Month',
+//         data:['1k','2k','3k'],
+
+//     }
+// })
